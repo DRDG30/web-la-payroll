0eeabde2f4110cb7c72f10141c4439843d15029c
DRDG30 <dylanreevegundran@gmail.com>
2024-09-09 16:23:40 +0800

    sas

--- admin/index.php
+++ admin/index.php
@@ -53,7 +53,7 @@ if (isset($_SESSION['admin'])) {
                             <div class="icheck-primary">
                                 <input type="checkbox" id="remember">
                                 <label for="remember">
-                                    Show Passwordd
+                                    Show Passworddddd
                                 </label>
                             </div>
                         </div>
