9d64d83bc5d38c86ec91430ea93593181e2a674e
DRDG30 <dylanreevegundran@gmail.com>
2024-09-10 17:37:36 +0800

    updated

--- admin/employee.php
+++ admin/employee.php
@@ -1,3 +1,4 @@
+<?php include 'includes/session.php'; ?>
 <?php include 'includes/header.php'; ?>
 <?php include 'includes/navbar.php'; ?>
 <?php include 'includes/sidebar.php'; ?>
--- admin/index.php
+++ admin/index.php
@@ -69,23 +69,16 @@ if (isset($_SESSION['admin'])) {
 
 
 
-                <!-- <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div> -->
-                <!-- /.social-auth-links -->
 
-                <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
+
+
+
+
+                <p class="mb-0 mt-3 float-right">Not Registered Yet |
+                    <a href="register.php">Register Here</a>
                 </p>
-                <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new admin</a>
-                </p>
+
+
             </div>
             <!-- /.login-card-body -->
         </div>
